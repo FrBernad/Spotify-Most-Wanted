b4f3e23d04fe21789767d15aa31952b6a509b6dd
Fran <frbernad@itba.edu.ar>
2021-12-10 17:20:41 -0300

    refactor: remove public dir

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./webapp/controllers/index');
+const usersRouter = require('./webapp/controllers/users');
 
 const app = express();
 
@@ -13,7 +13,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
