d9b2f812ca93b59b3f380ec99829dc5dcfcbe6b1
Fran <frbernad@itba.edu.ar>
2021-12-10 16:20:05 -0300

    fix: module name and csv to json array input data filename

--- datasets/population-scripts/csv_to_json_array.py
+++ datasets/population-scripts/csv_to_json_array.py
@@ -1,8 +1,8 @@
 import csv
 
-from song import Song
+from song_utils import Song
 
-input_CSV = open("../raw-data/database.csv", 'r', encoding="ISO-8859-1")
+input_CSV = open("../raw-data/Final database.csv", 'r', encoding="ISO-8859-1")
 
 output_json_array = open("database.json", "w", encoding="ISO-8859-1")
 
